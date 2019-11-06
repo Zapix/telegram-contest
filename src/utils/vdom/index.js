c85610ea7be973200cea3ae84bd2bf68d7287fa5
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2019-11-06 23:27:38 +0500

    Add demo rendering to index.js

--- src/utils/vdom/index.js
+++ src/utils/vdom/index.js
@@ -1,1 +1,2 @@
-
+export { default as createElement } from './createElement';
+export { default as render } from './render';
--- src/utils/vdom/isVNode.js
+++ src/utils/vdom/isVNode.js
@@ -1,4 +1,4 @@
-import R from 'ramda';
+import * as R from 'ramda';
 
 /**
  * Checks that passed js object has `__vnode` attribute
