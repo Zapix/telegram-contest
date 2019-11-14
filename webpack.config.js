e28d3b9488732946c07692e8c36b64d97a2c18ec
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2019-11-14 12:35:10 +0500

    Allow to write css as SASS in modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const process = require('process');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const mode  = process.env.NODE_ENV == 'production' ? 'production' : 'development'
+const mode = process.env.NODE_ENV == 'production' ? 'production' : 'development';
+const isDevelopment = mode === 'development';
 
 module.exports = {
   mode: mode,
@@ -16,6 +18,10 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Telegram Application',
+    }),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+      chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
     }),
   ],
   devtool: 'inline-source-map',
@@ -37,6 +43,39 @@ module.exports = {
           'file-loader',
         ]
       },
+      {
+        test: /\.module\.s(a|c)ss$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: isDevelopment
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      }
     ],
   },
   resolve: {
