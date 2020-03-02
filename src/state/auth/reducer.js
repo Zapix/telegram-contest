c3b477f44fbf764240d15a6a95ad78e0ffab261c
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2020-03-02 21:24:28 +0500

    Add auth phone pair

--- src/state/auth/reducer.js
+++ src/state/auth/reducer.js
@@ -3,6 +3,11 @@ import { isActionOf, buildReducer } from 'utils/store';
 
 import { AUTH_SEND_CODE, AUTH_SEND_CODE_ERROR, AUTH_SEND_CODE_SUCCESS } from './constants';
 
+const getCurrentPhonePair = R.pipe(
+  R.of,
+  R.ap([R.always('currentPhone'), R.prop('payload')]),
+);
+
 const handleAuthSendCode = R.always({ sendingAuthCode: true });
 const handleAuthSendCodeError = R.pipe(
   R.nth(1),
@@ -11,7 +16,12 @@ const handleAuthSendCodeError = R.pipe(
   R.of,
   R.fromPairs,
 );
-const handleAuthSendCodeSuccess = R.always({});
+const handleAuthSendCodeSuccess = R.pipe(
+  R.nth(1),
+  R.of,
+  R.ap([getCurrentPhonePair]),
+  R.fromPairs,
+);
 
 export default buildReducer({}, [
   [isActionOf(AUTH_SEND_CODE), handleAuthSendCode],
--- src/state/auth/reducer.test.js
+++ src/state/auth/reducer.test.js
@@ -39,8 +39,10 @@ describe('auth', () => {
 
   describe('AUTH_SEND_CODE_SUCCESS', () => {
     it('set success', () => {
-      const action = { type: AUTH_SEND_CODE_SUCCESS };
-      expect(reducer({ sendingAuthCode: true }, action)).toEqual({});
+      const action = { type: AUTH_SEND_CODE_SUCCESS, payload: '+79625213997' };
+      expect(reducer({ sendingAuthCode: true }, action)).toEqual({
+        currentPhone: '+79625213997',
+      });
     });
   });
 });
